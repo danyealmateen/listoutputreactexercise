958190f6f5e6899b07431c22e28d48d3a02ef21e
Danyeal Mateen <danyeal.mateen@hotmail.com>
2023-06-18 17:12:15 +0200

    exercise done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+
+const App = () => {
+
+  const theList = ["A", "B", "C", "D", "E", "F", "G", "H",];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Output List Excercise</h1>
+      {theList.map(item => {
+        console.log(item)
+        return <p key={item}>{item}</p>
+      })}
     </div>
-  );
+  )
 }
 
 export default App;
